517c4917af85779c8e7b3126b0c840419ca53b39
sulemankhan447 <sulemankhan447@gmail.com>
2018-10-20 19:59:12 +0530

    Design Completed With Call Start,end and message buttons with some animations on it

--- app/src/main/java/com/suleman/call/MainActivity.java
+++ app/src/main/java/com/suleman/call/MainActivity.java
@@ -1,46 +1,46 @@
 package com.suleman.call;
 
+import android.animation.AnimatorSet;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity{
 //    public class MainActivity extends AppCompatActivity implements Animation.AnimationListener{
     ImageView ivCallEnd;
+    ImageView ivCallStart;
+    ImageView ivMessage;
     Animation animMoveTop;
     Animation animMoveBottom;
+    AnimationSet animationSet;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ivCallEnd = (ImageView)findViewById(R.id.ivCallEnd);
+        ivCallStart = (ImageView)findViewById(R.id.ivCallStart);
+        ivMessage = (ImageView)findViewById(R.id.ivMessage);
+        animationSet = new AnimationSet(true);
         animMoveTop = AnimationUtils.loadAnimation(this,R.anim.move_top);
         animMoveBottom = AnimationUtils.loadAnimation(this,R.anim.move_bottom);
-        AnimationSet animationSet = new AnimationSet(true);
         animationSet.addAnimation(animMoveTop);
+
         animationSet.addAnimation(animMoveBottom);
-        ivCallEnd.startAnimation(animationSet);
-//        animMoveTop = AnimationUtils.loadAnimation(this,R.anim.move_top);
+        //        animationSet.setRepeatCount(1);
+//        animationSet.setRepeatMode(Animation.INFINITE);
+        animationSet.start();
+        ivCallStart.setAnimation(animationSet);
+        ivCallEnd.setAnimation(animationSet);
+        ivMessage.setAnimation(animationSet);
+
+        //        animMoveTop = AnimationUtils.loadAnimation(this,R.anim.move_top);
 //        animMoveTop.setAnimationListener(this);
 //        ivCallEnd.setAnimation(animMoveTop);
     }
-//
-//    @Override
-//    public void onAnimationStart(Animation animation) {
-//
-//    }
-//
-//    @Override
-//    public void onAnimationEnd(Animation animation) {
-//        Toast.makeText(MainActivity.this,"Animation Stopped",Toast.LENGTH_SHORT).show();
-//
-//    }
-//
-//    @Override
-//    public void onAnimationRepeat(Animation animation) {
-//
-//    }
 }
+
