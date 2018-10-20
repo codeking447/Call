949faac000543e3cd01dec5c13a1a1f46ba49d99
sulemankhan447 <sulemankhan447@gmail.com>
2018-10-20 18:18:37 +0530

    Call End Animation in Progress

--- app/src/main/java/com/suleman/call/MainActivity.java
+++ app/src/main/java/com/suleman/call/MainActivity.java
@@ -1,13 +1,46 @@
 package com.suleman.call;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity{
+//    public class MainActivity extends AppCompatActivity implements Animation.AnimationListener{
+    ImageView ivCallEnd;
+    Animation animMoveTop;
+    Animation animMoveBottom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ivCallEnd = (ImageView)findViewById(R.id.ivCallEnd);
+        animMoveTop = AnimationUtils.loadAnimation(this,R.anim.move_top);
+        animMoveBottom = AnimationUtils.loadAnimation(this,R.anim.move_bottom);
+        AnimationSet animationSet = new AnimationSet(true);
+        animationSet.addAnimation(animMoveTop);
+        animationSet.addAnimation(animMoveBottom);
+        ivCallEnd.startAnimation(animationSet);
+//        animMoveTop = AnimationUtils.loadAnimation(this,R.anim.move_top);
+//        animMoveTop.setAnimationListener(this);
+//        ivCallEnd.setAnimation(animMoveTop);
     }
+//
+//    @Override
+//    public void onAnimationStart(Animation animation) {
+//
+//    }
+//
+//    @Override
+//    public void onAnimationEnd(Animation animation) {
+//        Toast.makeText(MainActivity.this,"Animation Stopped",Toast.LENGTH_SHORT).show();
+//
+//    }
+//
+//    @Override
+//    public void onAnimationRepeat(Animation animation) {
+//
+//    }
 }
